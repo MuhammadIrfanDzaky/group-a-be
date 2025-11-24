473f8872eadf4cb218ae44b63e9e493e4d25ea8a
Faisal Firdaus <faisal.firdaus10.ff@gmail.com>
2025-11-24 17:34:45 +0700

    feat: add middleware

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -15,28 +15,13 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
+  @Post('review')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
-  }
-
-  @Get()
-  findAllProduct() {
-    return this.productsService.findAll();
   }
 
   @Get(':id')
   findOneProduct(@Param('id') id: string) {
     return this.productsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
   }
 }
